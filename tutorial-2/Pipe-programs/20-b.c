fc45f1bacf309773e33af5cc5804e052d42f8986
jaypanchal14 <71281672+jaypanchal14@users.noreply.github.com>
2023-10-18 01:09:08 +0530

    Update 20-b.c

--- tutorial-2/Pipe-programs/20-b.c
+++ tutorial-2/Pipe-programs/20-b.c
@@ -11,16 +11,16 @@ Objective : To have two-way communication using fifo with 20-a.c program.
 
 void main(){
 
-    char buf[100];
+    char buf[20];
     
     int fd;
     //Opening to readf from FIFO
-    if((fd = open("ff", O_RDONLY))){
+    if((fd = open("ff", O_RDONLY)) == -1){
         perror("Not able to open fifo");
         exit(EXIT_FAILURE);
     }
 
     read(fd, &buf, sizeof(buf));
 
-    printf("From 20-a.c : %s",buf);
+    printf("Message from PIPE : %s\n",buf);
 }
