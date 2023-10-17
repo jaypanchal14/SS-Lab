2a3d7fee55ae24fa0fd73378bbc8922e45373931
jaypanchal14 <71281672+jaypanchal14@users.noreply.github.com>
2023-10-18 02:03:54 +0530

    Update 30.c

--- tutorial-2/Shared-memory/30.c
+++ tutorial-2/Shared-memory/30.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -18,42 +19,62 @@ int main() {
     
     int key;
     // Getting unique key for the message queue
-    if ((key = ftok("/tmp", 'A')) == -1) {
+    if ((key = ftok("/tmp", 'C')) == -1) {
         perror("Error while generating key");
         exit(1);
     }
     int shm_id;
-    char *addr;
-    char data[] = "This is the data to be written to shared memory";
 
     // Create a shared memory segment with a specified key and permissions
     // size: 1024 bytes
-    if ((shm_id = shmget(key, 1024, IPC_CREAT | 0666)) == -1) {
+    if ((shm_id = shmget(key, 1024, IPC_CREAT | 0777)) == -1) {
         perror("Error while creating memory segment");
         exit(1);
     }
 
     // Attach to the shared memory segment for writing
-    addr = shmat(shm_id, NULL, 0);
-    if (addr == (char *)(-1)) {
+    char* addr = (char*)shmat(shm_id, NULL, 0);
+    if (addr == (void *)-1) {
         perror("Error while attaching memory segment");
         exit(1);
     }
+    char data[] = "This is the data to be written to shared memory";
 
     // Write data to shared memory
     strcpy(addr, data);
 
     printf("Data written to shared memory: %s\n", addr);
+    //Need to detach from the memory in order to update the flag
+    if(shmdt(addr) == -1) {
+            perror("Error while detaching from segment before updating the mode");
+    }
 
     // Updating with read-only access
-    addr = shmat(shm_id, NULL, SHM_RDONLY);
-    if (shmaddr == (char *)(-1)) {
+    addr = (char *)shmat(shm_id, NULL, SHM_RDONLY);
+    
+    if (addr == (void *)-1) {
         perror("Error while updating mode");
     }
 
-    // Write data to shared memory, it will fail
-    strcpy(addr, data);
-  
+    // Write data to shared memory, it will fail and give segmentation fault
+    //strcpy(addr, "Going to fail");
+
+    /*int pId = fork();
+    
+    if(pId == 0){
+        //Child process
+        // Write data to shared memory, it will fail and give segmentation fault
+        printf("Inside child process before writing: %s\n",addr);
+        strcpy(addr, "Going to fail");
+        printf("Inside child process: %s\n",addr);
+        exit(1);
+    }
+    
+    sleep(2);
+    printf("Before parent process: %s\n",addr);
+    strcpy(addr, "Going to fail");
+    printf("After parent process: %s\n",addr);*/
+
     // Detach the shared memory segment
     if(shmdt(addr) == -1) {
             perror("Error while detaching from segment");
