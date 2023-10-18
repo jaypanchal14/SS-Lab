c7269aa751b8417cd194c62a455a1e8d867b35f0
jaypanchal14 <71281672+jaypanchal14@users.noreply.github.com>
2023-10-18 16:19:19 +0530

    Create 32.c

---
+++ tutorial-2/Semaphore/32.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/*
+Objective : 
+Write a program to implement semaphore to protect any critical section.
+(a)rewrite the ticket number creation program using semaphore
+(b)protect shared memory from concurrent write access
+(c)protect multiple pseudo resources ( may be two) using counting semaphore
+(d)remove the created semaphore
+*/
+
+void main(){
+  
+}
