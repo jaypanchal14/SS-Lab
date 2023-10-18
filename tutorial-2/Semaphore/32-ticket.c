704a4842198edf2d0be7fb83c6139e69c4c67744
jaypanchal14 <71281672+jaypanchal14@users.noreply.github.com>
2023-10-18 18:40:29 +0530

    Update 32-ticket.c

--- tutorial-2/Semaphore/32-ticket.c
+++ tutorial-2/Semaphore/32-ticket.c
@@ -10,7 +10,6 @@
 Objective : 
 Write a program to implement semaphore to protect any critical section.
 ->rewrite the ticket number creation program using semaphore
-(d)remove the created semaphore
 */
 
 void initialise();
