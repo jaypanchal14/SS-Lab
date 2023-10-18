555a52283fdaea381a7ce41d1a4026db5b972fa4
jaypanchal14 <71281672+jaypanchal14@users.noreply.github.com>
2023-10-18 16:16:55 +0530

    Update 33-server.c

--- tutorial-2/Socket-programming/33-server.c
+++ tutorial-2/Socket-programming/33-server.c
@@ -10,17 +10,24 @@ Write a program to communicate between two machines using socket.
 This will act as server, and 33-client.c will act as client.
 */
 
+void handle_client(int socket);
+
 void main() {
     
     int server, client;
     struct sockaddr_in serverAddr, clientAddr;
     socklen_t addrL;
-    char buf[1024];
-    char* msg = "Hi client.";
+    
     // Creating socket with IPV4 and TCP channel
     server = socket(AF_INET, SOCK_STREAM, 0);
     if (server < 0) {
         perror("Error while creating socket");
+        exit(1);
+    }
+
+    int resuse = 1;
+    if(setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &resuse, sizeof(resuse)) == -1){
+        perror("Not able to set options in server-socket");
         exit(1);
     }
 
@@ -32,15 +39,41 @@ void main() {
         perror("Error in binding");
         exit(1);
     }
-    // Will respond to 3 max concurrently, others will wait/get error
-    listen(server, 3);
-    
+    // Will respond to 1 max concurrently, others will wait/get error
+    listen(server, 1);
+        
     addrL = sizeof(clientAddr);
     client = accept(server, (struct sockaddr *)&clientAddr, &addrL);
-    send(client, msg, strlen(msg), 0);
-    recv(client, &buf, 1024, 0);
-    printf("Message from client : %s",buf);
     
+    handle_client(client);
     close(client);
     close(server);
+}
+
+
+void handle_client(int client){
+
+    char buf[100];
+    char msg[100];
+    while(1){
+                
+        memset(buf, 0, sizeof(buf));
+        memset(msg, 0, sizeof(msg));
+        printf("Enter msg for client-%d : ",client);
+        scanf("%s",msg);
+        send(client, msg, strlen(msg), 0);
+        
+        int bytes = recv(client, &buf, 100, 0);
+        printf("Message from client: %s\n",buf);
+        if(strcmp(buf, "EXIT") == 0 || strcmp(buf, "exit") == 0){
+            printf("Going to close this client connection as exit message is received.\n");
+            break;
+        }
+        if(bytes < 1){
+            printf("Going to close this client connection as no bytes have been received.\n");
+            break;
+        }
+                    
+    }
+
 }
